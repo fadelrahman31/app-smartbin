e3213126774b43e5bad4c16a438d9e1af5a31af7
yanzahatta <yanza.hatta@gmail.com>
2020-04-17 11:42:15 +0700

    connected form device

---
+++ js/tambahdevicetps.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#formdevice').submit(function(event){
+        event.preventDefault();
+        var iddevice           = document.getElementById('iddevice').value;
+        var idtps              = document.getElementById('idtps').value;
+        var url = 'https://id-smartbin.herokuapp.com/device/'+iddevice;
+        console.log(iddevice);
+        console.log(idtps);
+        var dat = 
+        {
+            "id_device"         : iddevice,
+            "id_tps"           : idtps,
+            "is_connected"      : "true"
+        };
+        var data = JSON.stringify(dat);
+        $.ajax({
+           type: 'POST',
+           url: url,
+           contentType:"application/json",
+           data: data,
+           success: function(data){
+               alert(data); 
+               location.replace("device.html");
+           }
+         });
+    });
+
+});
