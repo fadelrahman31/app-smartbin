37dbbac9c85ec32e8ceb4896aef341ad668ab0d2
Claudia Renata <maharanic20@gmail.com>
2020-04-17 23:08:14 +0700

    add pengguna.html

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
-function toggleResetPswd(e){
-    e.preventDefault();
-    $('#logreg-forms .form-signin').toggle() // display:block or none
-    $('#logreg-forms .form-reset').toggle() // display:block or none
-}
+// function toggleResetPswd(e){
+//     e.preventDefault();
+//     $('#logreg-forms .form-signin').toggle() // display:block or none
+//     $('#logreg-forms .form-reset').toggle() // display:block or none
+// }
 
 function toggleSignUp(e){
     e.preventDefault();
@@ -12,8 +12,18 @@ function toggleSignUp(e){
 
 $(()=>{
     // Login Register Form
-    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
-    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    // $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    // $('#logreg-forms #cancel_reset').click(toggleResetPswd);
     $('#logreg-forms #btn-signup').click(toggleSignUp);
     $('#logreg-forms #cancel_signup').click(toggleSignUp);
 })
+
+document.getElementById("sign_in").onclick= function() {optChange()};
+function optChange(e) {
+    if ($("#opt1").is(":checked")){
+        window.location.replace("home.html");
+     }
+     else if (($("#opt2").is(":checked"))){
+        window.location.replace("pengguna.html");
+     }
+}
---
+++ js/pengguna.js
@@ -0,0 +1,30 @@
+var BASE_URL = "https://id-smartbin.herokuapp.com"
+
+
+$(document).ready(function(){
+    $.ajax({
+        url: "https://id-smartbin.herokuapp.com/user"
+    }).then(function(data) {
+        console.log(data);
+        // console.log(data.length);
+        // console.log(data[0]);
+        // console.log(data[0].length);
+        // console.log(data[0][1]);
+        // console.log(data[0][1]);
+        for (let i=0; i < data.length; i++){
+            let row = data[i];
+            const html = 
+            '<tr>\n'+
+            '    <td>' + row[0] + '</td>\n' +
+            '    <td>' + row[1] + '</td>\n' +
+            '    <td>' + row[2] + '</td>\n' +
+            '    <td>' + row[3] + '</td>\n' +
+            '    <td>' + row[4] + '</td>\n' +
+            '    <td>' + row[5] + '</td>\n' +
+            '    <td>' + row[6] + '</td>\n' +
+            '<td class="text-center">'+"<a class='btn btn-info btn-xs' "+'href="edittps.html?idtps='+row[0]+'"><span class="glyphicon glyphicon-edit"></span> Edit</a></td>' +
+            '</tr>';
+            $('#bodypengguna').append(html);
+        }
+    });
+});
